0b7614c87ee0f2737aeae72d23778524e473dbfb
Pedro Costa <costapedro.a1999@gmail.com>
2025-03-24 17:01:07 +0000

    Ruby: book-store

--- ruby/book-store/book_store.rb
+++ ruby/book-store/book_store.rb
@@ -1,7 +1,40 @@
-=begin
-Write your code for the 'Book Store' exercise in this file. Make the tests in
-`book_store_test.rb` pass.
+module BookStore
+  COSTS = {
+    1 => 8,
+    2 => 15.2,
+    3 => 21.6,
+    4 => 25.6,
+    5 => 30
+  }
 
-To get started with TDD, see the `README.md` file in your
-`ruby/book-store` directory.
-=end
+  def self.calculate_price(basket)
+    @memo = {}
+
+    min_price(basket)
+  end
+
+  def self.min_price(basket)
+    return 0 if basket.empty?
+
+    return @memo[basket] if @memo[basket]
+
+    min_price = Float::INFINITY
+    tally = basket.tally
+    options = tally.keys
+
+    1.upto(options.count).each do |bundle_size|
+      options.combination(bundle_size).each do |bundle|
+        new_basket = basket.dup
+        bundle.each { |book| new_basket.delete_at(new_basket.index(book)) }
+
+        current_price = COSTS[bundle_size] + min_price(new_basket)
+
+        min_price = [min_price, current_price].min
+      end
+    end
+
+    @memo[basket] = min_price
+
+    min_price
+  end
+end
